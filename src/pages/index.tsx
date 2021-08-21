a23f743c0d468f384219a816edc9897b6d95c162
黒川卓真 <skijump120@gmail.com>
2021-08-21 15:36:33 +0900

    1st commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,6 +36,7 @@ const Home = () => {
                 supabaseClient={client}
                 providers={["github"]}
                 socialColors={true}
+
                 />
               </div>
             </div>
