f85fc747803e544bbc05a2cf90ea155cb9d59d93
黒川卓真 <skijump120@gmail.com>
2021-08-22 12:57:21 +0900

    1st commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,71 @@
 import { Auth, Button, IconLogOut } from "@supabase/ui";
-import type { ReactNode } from "react";
-import React from "react";
+import React, { ReactNode, useCallback, useEffect, useState } from "react";
 import { LayoutWrapper } from "src/components/layoutWrapper";
+import { Title, TitleList } from "src/components/titleList";
 import { client } from "src/libs/supabase";
+
 type Props = {
   children: ReactNode;
 };
+
+const getTitles = async () => {
+  const { data, error } = await client
+    .from("manga_title")
+    .select("*")
+    .order("title");
+  if (!error && data) {
+    return data;
+  }
+  return [];
+};
+
 const Container = (props: Props) => {
   const { user } = Auth.useUser();
-// ログインしている場合
-if (user) {
+
+  const [text, setText] = useState<string>("");
+  const [titles, setTitles] = useState<Title[]>([]);
+
+  const getTitleList = useCallback(async () => {
+    const data = await getTitles();
+    setTitles(data);
+  }, []);
+
+  useEffect(() => {
+    getTitleList();
+  }, [user, getTitleList]);
+
+  if (user) {
     return (
       <div>
+        <div className="flex justify-center gap-2 p-4">
+          <input
+            className="w-full h-12 px-4 bg-white border border-gray-300 rounded shadow appearance-none hover:border-gray-700"
+            placeholder="Filtering text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <TitleList
+          titles={titles}
+          uuid={user.id}
+          getTitleList={getTitleList}
+          filterText={text}
+        />
         <div className="flex justify-end mx-2 my-4">
           <Button
             size="medium"
             icon={<IconLogOut />}
-            onClick={() => client.auth.signOut()}>
+            onClick={() => client.auth.signOut()}
+          >
             Sign out
           </Button>
         </div>
-</div> );
-}
-// ログインしていない場合
-return <>{props.children}</>;
+      </div>
+    );
+  }
+  return <>{props.children}</>;
 };
+
 const Home = () => {
   return (
     <LayoutWrapper>
@@ -36,13 +77,13 @@ const Home = () => {
                 supabaseClient={client}
                 providers={["github"]}
                 socialColors={true}
-
-                />
-              </div>
+              />
             </div>
-          </Container>
-        </Auth.UserContextProvider>
-      </LayoutWrapper>
-    );
-  };
-  export default Home;
+          </div>
+        </Container>
+      </Auth.UserContextProvider>
+    </LayoutWrapper>
+  );
+};
+
+export default Home;
